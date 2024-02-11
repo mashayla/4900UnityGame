cd23d7824996a883aeae0577b1180051b28d998e
Sofiat Okanlawon <89478783+SofiatAnike@users.noreply.github.com>
2024-02-11 01:07:02 -0500

    ADDED a line of code

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello from start");
     }
 
     // Update is called once per frame
